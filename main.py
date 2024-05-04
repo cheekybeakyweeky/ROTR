4aca7a6a2ee520ad8ccaf936fb82140d5f8ec646
cheekybeakyweeky <135086945+cheekybeakyweeky@users.noreply.github.com>
2024-05-04 18:10:58 -0500

    f

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("preride","preriderotor.mp4",.009,.2,4)
+    encoder.encode("preride","preriderotor.mp4",.003,.2,4)
 
 if __name__== "__main__":
     main()
